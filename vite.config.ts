c4fc2f85bf89b8c230337b37077e254994a06f06
tdd28 <tdd2810@gmail.com>
2024-09-08 16:56:48 +0700

    chore: config build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+  publicDir: false,
   build: {
     lib: {
-      entry: './lib/main.ts',
-      name: 'Counter',
-      fileName: 'counter'
+      entry: './lib/main.tsx',
+      name: 'Intl',
+      fileName: 'main'
+    },
+    rollupOptions: {
+      external: 'react',
+      output: {
+        globals: {
+          react: 'React'
+        }
+      }
     }
-  }
+  },
+  plugins: [
+    dts({ include: "lib" })
+  ]
 })
