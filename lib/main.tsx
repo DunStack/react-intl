f536518e8c8deaf56531a4e23b70b2bfcb0f02e5
tdd28 <tdd2810@gmail.com>
2024-09-08 16:25:51 +0700

    feat: translation params (#2)

--- lib/main.tsx
+++ lib/main.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react"
+import { createContext, useCallback, useContext } from "react"
 
 const LocaleContext = createContext<string | undefined>(undefined)
 
@@ -23,21 +23,52 @@ export function LocaleProvider({
   )
 }
 
-export function createIntl<Trans>(data: Record<string, Trans | undefined>) {
-  type Id = keyof Trans
+export function createIntl<Trans extends Record<string, string>>(data: {[K in string]?: Trans}) {
+  type TransId = keyof Trans
   
-  interface TProps {
-    id: Id
+  type TInfo = {
+    description?: string
   }
 
-  function useT(id: Id) {
+  type TParams<Id extends TransId> = Trans[Id] extends string
+    ? { params?: Record<string, TranValue>} 
+    : { params: Params<Trans[Id]> }
+
+  type TOptions<Id extends TransId> = TInfo & TParams<Id>
+  
+  type TProps<Id extends TransId> = { id: Id } & TOptions<Id>
+
+  type TArgs<Id extends TransId> = ParamKey<Trans[Id]> extends never 
+    ? [Id, TOptions<Id>?]
+    : [Id, TOptions<Id>]
+
+  function useT() {
     const locale = useLocale()
-    return data[locale]?.[id]
+    const t = useCallback(<Id extends TransId>(...args: TArgs<Id>) => {
+      const [id, opts] = args
+      const msg = data[locale]?.[id]
+      
+      if (msg && opts && 'params' in opts) {
+        return msg.replace(/\{(\w+)\}/g, (_, key: ParamKey<Trans[Id]>) => `${opts.params?.[key]}`)
+      }
+      
+      return msg
+    }, [locale]) 
+    return t
   }
 
-  function T({ id }: TProps) {
-    return useT(id)
+  function T<Id extends TransId>(props: TProps<Id>) {
+    const t =  useT()
+    return t(props.id, props)
   }
 
   return { useT, T }
 }
+
+type ParamKey<X extends string> = X extends `${string}{${infer P}}${infer R}` 
+  ? P | ParamKey<R>
+  : never
+
+type Params<X extends string> = Record<ParamKey<X>, TranValue>
+
+type TranValue = string | number
